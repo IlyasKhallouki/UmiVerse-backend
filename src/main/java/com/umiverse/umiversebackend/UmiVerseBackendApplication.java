273dde3383d08af6ac819e8a223b0651d0b8aa99
Ilyas Khallouki <khalloukielias@gmail.com>
2024-07-05 10:27:44 +0100

    First commit - clean spring project

---
+++ src/main/java/com/umiverse/umiversebackend/UmiVerseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.umiverse.umiversebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmiVerseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmiVerseBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/umiverse/umiversebackend/UmiVerseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umiverse.umiversebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmiVerseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
