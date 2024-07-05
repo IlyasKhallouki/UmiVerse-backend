29bb35df944acd062f2c08f7cd4fd30d16368210
Ilyas Khallouki <khalloukielias@gmail.com>
2024-07-05 12:01:44 +0100

    added register request  body

---
+++ src/main/java/com/umiverse/umiversebackend/body/RegisterRequestBody.java
@@ -0,0 +1,33 @@
+package com.umiverse.umiversebackend.body;
+
+public class RegisterRequestBody {
+    String username;
+    String fullName;
+    String email;
+    String password;
+    String role;
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
