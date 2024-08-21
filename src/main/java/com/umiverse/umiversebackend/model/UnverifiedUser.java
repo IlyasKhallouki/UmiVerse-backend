fcf30ef2816a74b3774efd64dd3ab2cb73285fe1
Ilyas Khallouki <khalloukielias@gmail.com>
2024-08-21 13:20:20 +0100

    Added the model for the unverified user

---
+++ src/main/java/com/umiverse/umiversebackend/model/UnverifiedUser.java
@@ -0,0 +1,82 @@
+package com.umiverse.umiversebackend.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.UUID;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "unverified_users")
+public class UnverifiedUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userID;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "bio")
+    private String bio;
+
+    @Column(name = "registration_date")
+    private Date registrationDate;
+
+    @Column(name = "role")
+    private String role;
+
+    @Column(name = "token_expiry_date")
+    private Timestamp tokenExpirationDate;
+
+    @Column(name = "verification_token")
+    private String verificationToken;
+
+
+    public UnverifiedUser() {}
+
+    public UnverifiedUser(String username, String password, String email, String fullName, String role, Timestamp tokenExpirationDate){
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.fullName = fullName;
+        this.bio = ":)";
+        this.role = role;
+        this.registrationDate = getCurrentDate();
+        this.tokenExpirationDate = null; // I don't know how to make it valid for 30 mins after registration
+        this.verificationToken = generateVerificationToken(); //
+    }
+
+    // get the current date when registering
+    private static Date getCurrentDate() {
+        long currentTimeMillis = System.currentTimeMillis();
+
+        return new Date(currentTimeMillis);
+    }
+
+    // get the token expiration date by adding 30 minutes to the current timestamp
+    private static Timestamp calculateTokenExpiryDate() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MINUTE, 30);
+        return new Timestamp(calendar.getTimeInMillis());
+    }
+
+    // generate a unique verification token
+    public static String generateVerificationToken() {
+        return UUID.randomUUID().toString();
+    }
+}
+
